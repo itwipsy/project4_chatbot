7eac7cf94e0273f412d9d4de49b4e6742fc89d8c
itwipsy <itwipsy17@gmail.com>
2022-01-28 19:35:20 +0900

    Upload_files

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import urllib.request
+import time
+import tensorflow_datasets as tfds
+import tensorflow as tf
+
+train_data = pd.read_csv('ChatbotData.csv')
+
